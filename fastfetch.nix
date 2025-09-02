933ecb498e31d1c3e3e1c03b7452e5a178fe25d1
Renan Bezerra Guimarães <renanbezerraguima@proton.me>
2025-09-02 09:42:44 -0300

    Created module

---
+++ fastfetch.nix
@@ -0,0 +1,93 @@
+{ config, pkgs, lib, ...}:
+
+{
+	programs.fastfetch = {
+		    enable = true;
+		    settings = {
+		      logo.source = "nixos";
+		
+		      modules = [
+		        "break"
+		        "separator"
+		
+		        # --- Hardware Section ---
+		        {
+		          type = "title";
+		          key = "Hardware Information ";
+		          keyColor = "yellow";
+		        }
+		        {
+		          type = "host";
+		          keyIcon = "󰌢";
+		          key = "󰌢";
+		        }
+		        {
+		          type = "cpu";
+		          keyIcon = "";
+		          key = "";
+		        }
+		        {
+		          type = "gpu";
+		          keyIcon = "󰍹";
+		          key = "󰍹";
+		        }
+		        {
+		         type = "memory";
+	             keyIcon = "󰍛";
+	             key = "󰍛";
+		        }
+		        {
+		         type = "disk";
+	    	     keyIcon = "󰋊";
+	    	     key = "󰋊";	
+		        }
+		
+		        "separator"
+		
+		        # --- Software Section ---
+		        {
+		          type = "title";
+		          key = "Software Information ";
+		          keyColor = "yellow";
+		        }
+		        {
+		          type = "os";
+		          keyIcon = "";
+		          key = "";
+		        }
+		        {
+		          type = "kernel";
+		          keyIcon = "󰌽";
+		          key = "󰌽";
+		        }
+		        {
+		          type = "wm";
+		          keyIcon = "󰖲";
+		          key = "󰖲";
+		        }
+		        {
+		          type = "shell";
+		          keyIcon = "";
+		          key = "";
+		        }
+		        {
+		          type = "terminal";
+		          keyIcon = "";
+		          key = "";
+		        }
+		        {
+		          type = "font";
+		          keyIcon = "";
+		          key = "";
+		        }
+		        {
+		          type = "packages";
+		          keyIcon = "󰏗";
+		          key = "󰏗";
+		        }
+		
+		        "separator"
+		      ];
+		  };
+		};
+}
--- home.nix
+++ home.nix
@@ -4,6 +4,10 @@
 	home.username = "renanbg";
 	home.homeDirectory = "/home/renanbg";
 
+	imports = [
+		./fastfetch.nix	
+	];
+
 	home.packages = with pkgs; [
 		protonvpn-gui
 		bat
@@ -47,96 +51,6 @@
 
 	programs.helix = {
 		enable = true;	
-	};
-
-	programs.fastfetch = {
-	    enable = true;
-	    settings = {
-	      logo.source = "nixos";
-	
-	      modules = [
-	        "break"
-	        "separator"
-	
-	        # --- Hardware Section ---
-	        {
-	          type = "title";
-	          key = "Hardware Information ";
-	          keyColor = "yellow";
-	        }
-	        {
-	          type = "host";
-	          keyIcon = "󰌢";
-	          key = "󰌢";
-	        }
-	        {
-	          type = "cpu";
-	          keyIcon = "";
-	          key = "";
-	        }
-	        {
-	          type = "gpu";
-	          keyIcon = "󰍹";
-	          key = "󰍹";
-	        }
-	        {
-	         type = "memory";
-             keyIcon = "󰍛";
-             key = "󰍛";
-	        }
-	        {
-	         type = "disk";
-    	     keyIcon = "󰋊";
-    	     key = "󰋊";	
-	        }
-	
-	        "separator"
-	
-	        # --- Software Section ---
-	        {
-	          type = "title";
-	          key = "Software Information ";
-	          keyColor = "yellow";
-	        }
-	        {
-	          type = "os";
-	          keyIcon = "";
-	          key = "";
-	        }
-	        {
-	          type = "kernel";
-	          keyIcon = "󰌽";
-	          key = "󰌽";
-	        }
-	        {
-	          type = "wm";
-	          keyIcon = "󰖲";
-	          key = "󰖲";
-	        }
-	        {
-	          type = "shell";
-	          keyIcon = "";
-	          key = "";
-	        }
-	        {
-	          type = "terminal";
-	          keyIcon = "";
-	          key = "";
-	        }
-	        {
-	          type = "font";
-	          keyIcon = "";
-	          key = "";
-	        }
-	        {
-	          type = "packages";
-	          keyIcon = "󰏗";
-	          key = "󰏗";
-	        }
-	
-	        "separator"
-	      ];
-	  };
 	};
 
 	programs.starship = {
